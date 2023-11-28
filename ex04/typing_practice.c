db5a192ed20cfa1353ca37f7a0b36cf855c8f3c5
sujiinnn <sujiinnn@gmail.com>
2023-11-28 20:24:42 +0900

    ex04 타자 연습 프로그램 구현

---
+++ ex04/typing_practice.c
@@ -0,0 +1,59 @@
+/*
+* 시스템 프로그래밍 Lab3 실습 4번
+* 파일 이름: typing_practice.c
+* 20213004 안수진
+*/
+
+#include <stdio.h>
+#include <termios.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#define PASSWORDSIZE 12
+
+int main(void)
+{
+    int fd;
+    int nread, cnt=0, errcnt=0;
+    char ch, text[] = "I want to go on a trip";
+    struct termios init_attr, new_attr;
+    time_t start_time, end_time; // 시작 시간, 종료 시간
+    
+    fd = open(ttyname(fileno(stdin)), O_RDWR);
+    tcgetattr(fd, &init_attr);
+
+    new_attr = init_attr;
+    new_attr.c_lflag &= ~ICANON;
+    new_attr.c_lflag &= ~ECHO;
+
+    new_attr.c_cc[VMIN] = 1;
+    new_attr.c_cc[VTIME] = 0;
+
+    if (tcsetattr(fd, TCSANOW, &new_attr) != 0) {
+        fprintf(stderr, "터미널 속성을 설정할 수 없음.\n");
+    }
+    printf("다음 문장을 그대로 입력하세요.\n%s\n", text);
+    time(&start_time); // 시작 시간
+
+    while ((nread=read(fd, &ch, 1)) > 0 && ch != '\n') {
+        if (ch == text[cnt++])
+            write(fd, &ch, 1);
+        else {
+            write(fd, "*", 1);
+            errcnt++;
+        }
+    }
+    time(&end_time); // 종료 시간
+
+    printf("\n타이핑 오류의 횟수는 %d\n", errcnt);
+    printf("분당 타자수는 : %.f\n", (float)(strlen(text)) / ((float)(end_time - start_time)) * 60.0);
+
+    tcsetattr(fd, TCSANOW, &init_attr);
+    close(fd);
+
+    return 0;
+}
